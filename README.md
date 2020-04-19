20ea8ed7871e11e9cead5d8c2c2de5e58f341af2
Andrew <hayhurst.andrew@gmail.com>
2020-04-18 17:50:31 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # root-boot-glute
+
+I adapted for a friend my Rock Paper Scissors project.
+
+Major script change was replacing
+
+rockButton.onclick = function(){playRound(0, computerPlay())};
+paperButton.onclick = function(){playRound(1, computerPlay())};
+scissorsButton.onclick = function(){playRound(2, computerPlay())};
+
+with
+
+window.addEventListener('click', respondToClick);
+function respondToClick(e) {
+  const playerSelection = e.target.dataset.choice;
+  const computerSelection = computerPlay();
+
+  if(!playerSelection) return;
+
+  playRound(playerSelection, computerSelection);
+}
+
+window.removeEventListener('click', respondToClick);
+
+This allowed me to remove button responsiveness after the last game of the match. I also changed computerPlay to return strings instead of numbers, which made the code more readable.
+
+I also needed to give the buttons a specific class where I made the effects with :hover and :active. I temporarily removed the class from these buttons so they looked inactive to user.
---
+++ script-game.js
@@ -0,0 +1,136 @@
+function computerPlay() {
+  let number = Math.floor(Math.random()*3); //0=rock; 1=paper; 2=scissors;
+  return (number == 0) ? 'root' : (number == 1) ? 'boot' : 'glute';
+}
+
+function playRound(playerSelection, computerSelection) {
+  if(roundNumber==0) {
+    instructions.remove();
+    currentRound.appendChild(userImage);
+    vs.textContent = "VS";
+    currentRound.appendChild(vs);
+    currentRound.appendChild(computerImage);
+    userPoint.forEach(item => {
+      item.classList.add('far');
+      item.classList.remove('fas');
+    })
+    computerPoint.forEach(item => {
+      item.classList.add('far');
+      item.classList.remove('fas');
+    })
+  }
+  roundNumber++;
+
+  userImage.src = `./${playerSelection}.png`;
+  computerImage.src = `./${computerSelection}.png`;
+
+  switch(playerSelection){
+    case('root'):
+      (computerSelection == 'boot') ? game("You win! Boot trips on Root!")
+      :(computerSelection == 'glute') ? game("You lose! Glute sits on Root!") 
+      : game("It's a tie!");
+      break;
+    case('boot'):
+      (computerSelection == 'glute') ? game("You win! Boot kicks Glute!")
+      :(computerSelection == 'root') ? game("You lose! Boot trips on Root!") 
+      : game("It's a tie!");
+      break;
+    case('glute'):
+      (computerSelection == 'root') ? game("You win! Glute sits on Root!")
+      :(computerSelection == 'boot') ? game("You lose! Boot kicks Glute!") 
+      : game("It's a tie!");
+      break;
+    default:
+      console.error('playerSelection did not equal 0, 1, or 2');
+  }
+}
+
+function game(roundResult) {
+  const roundDescription = document.querySelector('#round-description');
+  roundDescription.textContent = `Round ${roundNumber}: ${roundResult}`
+  userImage.style.border = '4px solid transparent';
+  computerImage.style.border = '4px solid transparent';
+  
+  if(roundResult.indexOf("win") !== -1){
+    userScore++;
+    userImage.style.border = '4px solid red';
+    userPoint[userScore-1].classList.add('fas');
+    userPoint[userScore-1].classList.remove('far');
+  }else if(roundResult.indexOf("lose") !== -1){
+    computerScore++;
+    computerImage.style.border = '4px solid red';
+    computerPoint[computerScore-1].classList.add('fas');
+    computerPoint[computerScore-1].classList.remove('far');
+  }
+  else if(roundResult.indexOf("tie") !== -1){
+    //do nothing
+  }else{
+    console.error("Incorrect input");
+  }
+  
+  if(userScore==5 || computerScore==5) {
+    window.removeEventListener('click', respondToClick);
+    rockButton.classList.remove('button-action');
+    scissorsButton.classList.remove('button-action');
+    paperButton.classList.remove('button-action');
+    let delayInMilliseconds = 3000;
+    setTimeout(function() {
+      if(userScore==5) instructions.textContent = "You won the match! Make your choice to play again...";
+      if(computerScore==5) instructions.textContent = "You lost the match. Make your choice to play again...";
+      roundNumber = 0;
+      userScore = 0;
+      computerScore = 0;
+      currentRound.appendChild(instructions);
+      currentRound.removeChild(userImage);
+      currentRound.removeChild(vs);
+      currentRound.removeChild(computerImage);
+      roundDescription.textContent = "";
+      window.addEventListener('click', respondToClick);
+      rockButton.classList.add('button-action');
+      scissorsButton.classList.add('button-action');
+      paperButton.classList.add('button-action');
+    }, delayInMilliseconds);
+  }
+}
+
+let rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+
+const userPoint = [];
+userPoint[0] = document.querySelector('#user-point1');
+userPoint[1] = document.querySelector('#user-point2');
+userPoint[2] = document.querySelector('#user-point3');
+userPoint[3] = document.querySelector('#user-point4');
+userPoint[4] = document.querySelector('#user-point5');
+const computerPoint = [];
+computerPoint[0] = document.querySelector('#computer-point1');
+computerPoint[1] = document.querySelector('#computer-point2');
+computerPoint[2] = document.querySelector('#computer-point3');
+computerPoint[3] = document.querySelector('#computer-point4');
+computerPoint[4] = document.querySelector('#computer-point5');
+
+let roundNumber = 0;
+let userScore = 0;
+let computerScore = 0;
+
+let currentRound = document.querySelector('#current-round');
+let instructions = document.querySelector('#instructions');
+let userImage = document.createElement('img');
+userImage.classList.add('round-images');
+let computerImage = document.createElement('img');
+computerImage.classList.add('round-images');
+let vs = document.createElement('span');
+vs.setAttribute('id', 'vs');
+
+window.addEventListener('click', respondToClick);
+function respondToClick(e) {
+  const playerSelection = e.target.dataset.choice;
+  const computerSelection = computerPlay();
+
+  if(!playerSelection) return;
+
+  playRound(playerSelection, computerSelection);
+}
+
+const roundDescription = document.querySelector('#round-description');
